cd4c9295840b6e285153e07618bb1023f2bbea00
Adam-Barto <adam.jbarto@gmail.com>
2023-03-14 17:25:56 -0400

    fizz Buzz complete

---
+++ fizz_buzz.py
@@ -0,0 +1,11 @@
+for x in range(100):
+    if x % 3 == 0 == x % 5:
+        print('FizzBuzz')
+    elif x % 3 == 0:
+        print('Fizz')
+    elif x % 5 == 0:
+        print('Buzz')
+    else:
+        print(x)
+
+        
